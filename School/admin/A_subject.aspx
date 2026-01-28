7e2e22e82fec17c9037e99339bdfce8a7d629b86
Madhura <madhura@example.com>
2026-01-28 22:59:56 +0530

    add the subject page

---
+++ School/admin/A_subject.aspx
@@ -0,0 +1,126 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/admin/Site1.Master" AutoEventWireup="true" CodeBehind="A_subject.aspx.cs" Inherits="School.admin.A_subject" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="content-body">
+        <div class="container-fluid mt-4">
+            <h5>📚 Subject Allocation</h5>
+
+            <!-- ADD FORM -->
+            <div class="card mb-4">
+                <div class="card-body">
+                    <div class="row">
+
+                        <div class="col-md-4 mb-3">
+                            <label>Class</label>
+                            <asp:DropDownList ID="ddlClass" runat="server" CssClass="form-control"></asp:DropDownList>
+                        </div>
+
+                        <div class="col-md-4 mb-3">
+                            <label>Section</label>
+                            <asp:DropDownList ID="ddlSection" runat="server" CssClass="form-control"></asp:DropDownList>
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <label>Subject</label>
+                            <asp:DropDownList ID="ddlSubject" runat="server" CssClass="form-control" />
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <label>Teacher</label>
+                            <asp:DropDownList ID="ddlTeacher" runat="server" CssClass="form-control" />
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <label>Academic Year</label>
+                            <asp:DropDownList ID="ddlAcademicYear" runat="server" CssClass="form-control">
+                                <asp:ListItem Text="-- Select --" Value=""></asp:ListItem>
+                                <asp:ListItem>2024-25</asp:ListItem>
+                                <asp:ListItem>2025-26</asp:ListItem>
+                                <asp:ListItem>2026-27</asp:ListItem>
+                            </asp:DropDownList>
+                        </div>
+
+                    </div>
+
+                    <asp:Button ID="btnSave" runat="server"
+                        Text="Save"
+                        CssClass="btn btn-primary"
+                        OnClick="btnSave_Click" />
+                </div>
+            </div>
+
+            <!-- GRID -->
+            <div class="card">
+                <div class="card-body">
+                    <asp:GridView ID="gvSubjects" runat="server"
+                        AutoGenerateColumns="False"
+                        CssClass="table table-bordered mt-4"
+                        AllowPaging="true"
+                        PageSize="10"
+                        OnPageIndexChanging="gvSubjects_PageIndexChanging"
+                        OnRowCommand="gvSubjects_RowCommand">
+
+                        <Columns>
+                            <asp:BoundField DataField="ClassName" HeaderText="Class" />
+                            <asp:BoundField DataField="SectionName" HeaderText="Section" />
+                            <asp:BoundField DataField="SubjectName" HeaderText="Subject" />
+                            <asp:BoundField DataField="TeacherName" HeaderText="Teacher" />
+                            <asp:BoundField DataField="AcademicYear" HeaderText="Year" />
+
+                            <asp:TemplateField HeaderText="Action">
+                                <ItemTemplate>
+                                    <asp:LinkButton ID="btnEdit" runat="server"
+                                        CommandName="EditRow"
+                                        CommandArgument='<%# Eval("CSA_ID") %>'
+                                        CssClass="btn btn-sm btn-primary">
+                                       Edit
+                                    </asp:LinkButton>
+
+                                    <asp:LinkButton ID="btnDelete" runat="server"
+                                        CommandName="DeleteRow"
+                                        CommandArgument='<%# Eval("CSA_ID") %>'
+                                        CssClass="btn btn-sm btn-danger"
+                                        OnClientClick="return confirm('Are you sure you want to delete?');">
+                                         Delete
+                                    </asp:LinkButton>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+
+                    <!-- EDIT MODAL -->
+                    <div class="modal fade" id="editModal" tabindex="-1">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h5>Edit Subject Allocation</h5>
+                                    <asp:LinkButton ID="btnCloseModal" runat="server" CssClass="btn-close" OnClick="btnCloseModal_Click"> </asp:LinkButton>
+                                </div>
+
+                                <div class="modal-body">
+                                    <asp:HiddenField ID="hfCSAID" runat="server" />
+
+                                    <asp:DropDownList ID="ddlEditSubject" runat="server" CssClass="form-control mb-2" />
+                                    <asp:DropDownList ID="ddlEditTeacher" runat="server" CssClass="form-control mb-2" />
+                                </div>
+
+                                <div class="modal-footer">
+                                    <asp:Button ID="btnUpdate" runat="server"
+                                        Text="Update"
+                                        CssClass="btn btn-success"
+                                        OnClick="btnUpdate_Click" />
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+</asp:Content>
---
+++ School/admin/A_subject.aspx.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace School.admin
+{
+    public partial class A_subject : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(
+    ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindSubject();
+                BindTeacher();
+                BindGrid();
+                BindSection();
+                BindClass();
+                BindEditSubject();
+            }
+        }
+        void BindSubject()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT SubjectID, SubjectName FROM SubjectMaster", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlSubject.DataSource = dt;
+            ddlSubject.DataTextField = "SubjectName";
+            ddlSubject.DataValueField = "SubjectID";
+            ddlSubject.DataBind();
+            ddlSubject.Items.Insert(0, "-- Select Subject --");
+        }
+        void BindClass()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT ClassID, ClassName FROM classMaster", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlClass.DataSource = dt;
+            ddlClass.DataTextField = "ClassName";
+            ddlClass.DataValueField = "ClassID";
+            ddlClass.DataBind();
+            ddlClass.Items.Insert(0, "-- Select Class --");
+        }
+
+        void BindSection()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT SectionID, SectionName FROM sectionMaster", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlSection.DataSource = dt;
+            ddlSection.DataTextField = "SectionName";
+            ddlSection.DataValueField = "SectionID";
+            ddlSection.DataBind();
+            ddlSection.Items.Insert(0, "-- Select Section --");
+        }
+
+        void BindTeacher()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT TeacherID, TeacherName FROM add_teacher", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlTeacher.DataSource = dt;
+            ddlTeacher.DataTextField = "TeacherName";
+            ddlTeacher.DataValueField = "TeacherID";
+            ddlTeacher.DataBind();
+            ddlTeacher.Items.Insert(0, "-- Select Teacher --");
+        }
+        protected void gvSubjects_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvSubjects.PageIndex = e.NewPageIndex;
+            BindGrid();
+        }
+
+        void BindGrid()
+        {
+            string q = @"
+        SELECT 
+            a.CSA_ID,
+            c.ClassName,
+            s.SectionName,
+            sm.SubjectName,
+            t.TeacherName,
+            a.AcademicYear
+        FROM ClassSubjectAllocation a
+        JOIN classMaster c ON a.ClassID = c.ClassID
+        JOIN sectionMaster s ON a.SectionID = s.SectionID
+        JOIN SubjectMaster sm ON a.SubjectID = sm.SubjectID
+        JOIN add_teacher t ON a.TeacherID = t.TeacherID
+        WHERE a.IsActive = 1";
+
+            SqlDataAdapter da = new SqlDataAdapter(q, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gvSubjects.DataSource = dt;
+            gvSubjects.DataBind();
+        }
+
+        bool IsDuplicate()
+        {
+            SqlCommand cmd = new SqlCommand(@"
+        SELECT COUNT(*) FROM ClassSubjectAllocation
+        WHERE ClassID=@ClassID
+        AND SectionID=@SectionID
+        AND SubjectID=@SubjectID
+        AND AcademicYear=@Year
+        AND IsActive=1", con);
+
+            cmd.Parameters.AddWithValue("@ClassID", ddlClass.SelectedValue);
+            cmd.Parameters.AddWithValue("@SectionID", ddlSection.SelectedValue);
+            cmd.Parameters.AddWithValue("@SubjectID", ddlSubject.SelectedValue);
+            cmd.Parameters.AddWithValue("@Year", ddlAcademicYear.SelectedValue);
+
+            con.Open();
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+            con.Close();
+
+            return count > 0;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (IsDuplicate())
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(),
+                    "dup1", "alert('Subject already allocated for this class & section');", true);
+                return;
+            }
+
+            if (IsTeacherDuplicate())
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(),
+                    "dup2", "alert('Teacher already allocated for this class & section');", true);
+                return;
+            }
+
+            SqlCommand cmd = new SqlCommand(@"
+        INSERT INTO ClassSubjectAllocation
+        (ClassID, SectionID, SubjectID, TeacherID, AcademicYear, IsActive)
+        VALUES (@C,@S,@Sub,@T,@Y,1)", con);
+
+            cmd.Parameters.AddWithValue("@C", ddlClass.SelectedValue);
+            cmd.Parameters.AddWithValue("@S", ddlSection.SelectedValue);
+            cmd.Parameters.AddWithValue("@Sub", ddlSubject.SelectedValue);
+            cmd.Parameters.AddWithValue("@T", ddlTeacher.SelectedValue);
+            cmd.Parameters.AddWithValue("@Y", ddlAcademicYear.SelectedValue);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            BindGrid();
+        }
+
+
+        protected void gvSubjects_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "EditRow")
+            {
+                hfCSAID.Value = e.CommandArgument.ToString();
+
+                BindEditSubject();
+                BindEditTeacher();
+
+                SqlCommand cmd = new SqlCommand(
+                    "SELECT SubjectID, TeacherID FROM ClassSubjectAllocation WHERE CSA_ID=@ID", con);
+                cmd.Parameters.AddWithValue("@ID", hfCSAID.Value);
+
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    ddlEditSubject.SelectedValue = dr["SubjectID"].ToString();
+                    ddlEditTeacher.SelectedValue = dr["TeacherID"].ToString();
+                }
+                con.Close();
+
+                ScriptManager.RegisterStartupScript(this, GetType(),
+                    "show", "$('#editModal').modal('show');", true);
+            }
+
+            else if (e.CommandName == "DeleteRow")
+            {
+                SqlCommand cmd = new SqlCommand(
+                    "UPDATE ClassSubjectAllocation SET IsActive=0 WHERE CSA_ID=@ID", con);
+                cmd.Parameters.AddWithValue("@ID", e.CommandArgument);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                BindGrid();
+            }
+        }
+        bool IsTeacherDuplicate()
+        {
+            SqlCommand cmd = new SqlCommand(@"
+        SELECT COUNT(*) FROM ClassSubjectAllocation
+        WHERE ClassID=@C
+        AND SectionID=@S
+        AND TeacherID=@T
+        AND AcademicYear=@Y
+        AND IsActive=1", con);
+
+            cmd.Parameters.AddWithValue("@C", ddlClass.SelectedValue);
+            cmd.Parameters.AddWithValue("@S", ddlSection.SelectedValue);
+            cmd.Parameters.AddWithValue("@T", ddlTeacher.SelectedValue);
+            cmd.Parameters.AddWithValue("@Y", ddlAcademicYear.SelectedValue);
+
+            con.Open();
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+            con.Close();
+
+            return count > 0;
+        }
+
+
+        void BindEditSubject()
+        {
+            SqlDataAdapter da = new SqlDataAdapter(
+                "SELECT SubjectID, SubjectName FROM SubjectMaster", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlEditSubject.DataSource = dt;
+            ddlEditSubject.DataTextField = "SubjectName";
+            ddlEditSubject.DataValueField = "SubjectID";
+            ddlEditSubject.DataBind();
+        }
+
+        void BindEditTeacher()
+        {
+            SqlDataAdapter da = new SqlDataAdapter(
+                "SELECT TeacherID, TeacherName FROM add_teacher", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlEditTeacher.DataSource = dt;
+            ddlEditTeacher.DataTextField = "TeacherName";
+            ddlEditTeacher.DataValueField = "TeacherID";
+            ddlEditTeacher.DataBind();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand(@"
+        UPDATE ClassSubjectAllocation
+        SET SubjectID=@Sub, TeacherID=@T
+        WHERE CSA_ID=@ID", con);
+
+            cmd.Parameters.AddWithValue("@Sub", ddlEditSubject.SelectedValue);
+            cmd.Parameters.AddWithValue("@T", ddlEditTeacher.SelectedValue);
+            cmd.Parameters.AddWithValue("@ID", hfCSAID.Value);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            BindGrid();
+
+            ScriptManager.RegisterStartupScript(this, GetType(),
+                "hide", "$('#editModal').modal('hide');", true);
+        }
+        protected void btnCloseModal_Click(object sender, EventArgs e)
+        {
+            ScriptManager.RegisterStartupScript(
+                this,
+                GetType(),
+                "CloseModal",
+                @"
+        var modalEl = document.getElementById('editModal');
+        var modal = bootstrap.Modal.getInstance(modalEl);
+        if (modal) {
+            modal.hide();
+        }
+        ",
+                true
+            );
+        }
+
+    }
+}
