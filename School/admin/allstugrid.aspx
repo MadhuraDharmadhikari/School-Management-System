2862cfffdee5ccef124833e44e9600086b9cdbc2
Madhura <madhura@example.com>
2026-03-08 15:01:52 +0530

    new allstugrid is genrated

---
+++ School/admin/allstugrid.aspx
@@ -0,0 +1,147 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/admin/Site1.Master" AutoEventWireup="true" CodeBehind="allstugrid.aspx.cs" Inherits="School.admin.allstugrid" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div class="content-body">
+
+        <!-- row -->
+             <!-- Search Filters -->
+            <div class="card m-4">
+                <div class="card-body">
+                    <h5 class="card-title">Search Students</h5>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <asp:TextBox ID="txtSearchName" runat="server" CssClass="form-control" Placeholder="Student Name"></asp:TextBox>
+                        </div>
+                        <div class="col-md-3">
+                            <asp:DropDownList ID="ddlSearchClass" runat="server" CssClass="form-control">
+                                <asp:ListItem Text="-- Select Class --" Value="" />
+                            </asp:DropDownList>
+                        </div>
+                        <div class="col-md-3">
+                            <asp:DropDownList ID="ddlSearchSection" runat="server" CssClass="form-control">
+                                <asp:ListItem Text="-- Select Section --" Value="" />
+                            </asp:DropDownList>
+                        </div>
+                        <div class="col-md-3">
+                            <asp:Button ID="btnSearch" runat="server" Text="Search" CssClass="btn btn-primary" OnClick="btnSearch_Click" />
+                            <asp:Button ID="btnReset" runat="server" Text="Reset" CssClass="btn btn-secondary ml-2" OnClick="btnReset_Click" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+               <div class="card m-3">
+                <div class="card-body">
+                    <h3 class="card-title mb-4">Conform Students</h3>
+                 <%--   Gridview--%>
+                    <div class="table-responsive">
+                <asp:GridView ID="gvStudents" runat="server"
+    CssClass="table table-bordered table-striped"
+    AutoGenerateColumns="False"
+    DataKeyNames="StudentID"
+    AllowPaging="True"
+    PageSize="10"
+
+    OnPageIndexChanging="gvStudents_PageIndexChanging"
+    OnRowEditing="gvStudents_RowEditing"
+    OnRowUpdating="gvStudents_RowUpdating"
+    OnRowCancelingEdit="gvStudents_RowCancelingEdit"
+    OnRowDeleting="gvStudents_RowDeleting"
+    OnRowCommand="gvStudents_RowCommand">
+                        
+                        <Columns>
+                                   
+                            <asp:BoundField DataField="AdmissionNo" HeaderText="Admission No" />
+                              <asp:BoundField DataField="StudentName" HeaderText="Student Name" />
+                            <asp:BoundField DataField="Class" HeaderText="Class" />
+                             <asp:BoundField DataField="ParentPhone" HeaderText="Parent Phone" />
+                             <asp:BoundField DataField="AdmissionStatus" HeaderText="Admission Status" />
+
+<%--                            <asp:BoundField DataField="PaymentMode" HeaderText="Payment Mode" />--%>
+
+                                   
+        <%--<asp:TemplateField HeaderText="Action">
+            <ItemTemplate>
+                <asp:LinkButton ID="btnEdit" runat="server"
+                    CssClass="btn btn-sm btn-primary mb-2"
+                    CommandName="EditStudent"
+                    CommandArgument='<%# Eval("StudentID") %>'>
+                    Edit
+                </asp:LinkButton>
+
+                <asp:LinkButton ID="btnDelete" runat="server"
+                    CssClass="btn btn-sm btn-danger "
+                    CommandName="Delete"
+                    CommandArgument='<%# Eval("StudentID") %>'
+                    OnClientClick="return confirm('Are you sure?');">
+                    Delete
+                </asp:LinkButton>
+            </ItemTemplate>
+        </asp:TemplateField>--%>
+
+                        </Columns>
+                    </asp:GridView>
+                        </div>
+                    <!-- EDIT MODAL -->
+<div class="modal fade" id="editModal" tabindex="-1">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h5 class="modal-title">Edit Student</h5>
+               <asp:LinkButton ID="btnCloseModal" runat="server" CssClass="btn-close"  OnClick="btnCloseModal_Click"> </asp:LinkButton>
+            </div>
+
+            <div class="modal-body">
+
+                <asp:HiddenField ID="hfStudentID" runat="server" />
+                <div class="row">
+                   <div class="col-md-4 mb-3">
+       <asp:Label ID="Label5" runat="server" Text="Admission No" Font-Bold="true" Style="margin-top: 10px"></asp:Label>
+       <asp:TextBox ID="txtaddno" CssClass="form-control card-title" runat="server" Text="" Style="margin-top: 10px" ReadOnly="true"></asp:TextBox>
+           </div>
+
+                <div class="mb-3 col-md-4 ">
+                    <asp:Label ID="Label1" runat="server" Text="Student Name" Font-Bold="true" Style="margin-top: 10px"></asp:Label>
+                    <asp:TextBox ID="txtStudentName" runat="server" CssClass="form-control" Style="margin-top: 10px" />
+                </div>
+
+                <div class="mb-3 col-md-4">
+                   <asp:Label ID="Label2" runat="server" Text="Class" Font-Bold="true" Style="margin-top: 10px"></asp:Label>
+                    <asp:TextBox ID="txtClass" runat="server" CssClass="form-control" Style="margin-top: 10px"/>
+                </div>
+
+                <div class="mb-3 col-md-4">
+                        <asp:Label ID="Label3" runat="server" Text="Total Fees" Font-Bold="true" Style="margin-top: 10px"></asp:Label>
+                    <asp:TextBox ID="txtTotalFees" runat="server" CssClass="form-control" Style="margin-top: 10px"  />
+                </div>
+
+                <div class="mb-3 col-md-4">
+                        <asp:Label ID="Label4" runat="server" Text="Paid Amount" Font-Bold="true" Style="margin-top: 10px"></asp:Label>
+                    <asp:TextBox ID="txtPaidAmount" runat="server" CssClass="form-control" Style="margin-top: 10px"  OnTextChanged="txtPaidAmount_TextChanged"  AutoPostBack="true"/>
+                </div>
+
+                <div class="mb-3 col-md-4">
+                        <asp:Label ID="Label6" runat="server" Text="Pending Amount" Font-Bold="true" Style="margin-top: 10px"></asp:Label>
+                    <asp:TextBox ID="txtPaymentMode" runat="server" CssClass="form-control" Style="margin-top: 10px" ReadOnly="true"/>
+                </div>
+</div>
+            </div>
+
+            <div class="modal-footer">
+                <asp:Button ID="btnUpdateStudent" runat="server"
+                    CssClass="btn btn-success"
+                    Text="Update"
+                    OnClick="btnUpdateStudent_Click" />
+            </div>
+
+        </div>
+    </div>
+</div>
+
+                </div>
+            </div>
+        </div>
+  
+            
+</asp:Content>
---
+++ School/admin/allstugrid.aspx.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace School.admin
+{
+    public partial class allstugrid : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString);
+        SqlCommand cmd;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindClassDropdown();
+                BindSectionDropdown();
+                BindStudentGrid();
+            }
+        }
+
+        private void BindClassDropdown()
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT ClassID, ClassName FROM ClassMaster", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                ddlSearchClass.DataSource = dt;
+                ddlSearchClass.DataTextField = "ClassName";
+                ddlSearchClass.DataValueField = "ClassName";
+                ddlSearchClass.DataBind();
+                ddlSearchClass.Items.Insert(0, new System.Web.UI.WebControls.ListItem("-- Select Class --", ""));
+            }
+        }
+
+        private void BindSectionDropdown()
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT SectionID, SectionName FROM SectionMaster", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                ddlSearchSection.DataSource = dt;
+                ddlSearchSection.DataTextField = "SectionName";
+                ddlSearchSection.DataValueField = "SectionName";
+                ddlSearchSection.DataBind();
+                ddlSearchSection.Items.Insert(0, new System.Web.UI.WebControls.ListItem("-- Select Section --", ""));
+            }
+        }
+
+        private void BindStudentGrid(string nameFilter = "", string classFilter = "", string sectionFilter = "")
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString))
+            {
+                string query = "SELECT * FROM Add_Student WHERE 1=1";
+                if (!string.IsNullOrEmpty(nameFilter))
+                    query += " AND StudentName LIKE @Name";
+                if (!string.IsNullOrEmpty(classFilter))
+                    query += " AND Class = @Class";
+                if (!string.IsNullOrEmpty(sectionFilter))
+                    query += " AND Section = @Section";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                if (!string.IsNullOrEmpty(nameFilter))
+                    cmd.Parameters.AddWithValue("@Name", "%" + nameFilter + "%");
+                if (!string.IsNullOrEmpty(classFilter))
+                    cmd.Parameters.AddWithValue("@Class", classFilter);
+                if (!string.IsNullOrEmpty(sectionFilter))
+                    cmd.Parameters.AddWithValue("@Section", sectionFilter);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvStudents.DataSource = dt;
+                gvStudents.DataBind();
+            }
+        }
+
+        // GridView Paging
+        protected void gvStudents_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvStudents.PageIndex = e.NewPageIndex;
+            BindStudentGrid(txtSearchName.Text.Trim(),
+                            ddlSearchClass.SelectedValue,
+                            ddlSearchSection.SelectedValue);
+        }
+
+        // Search button
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            BindStudentGrid(txtSearchName.Text.Trim(), ddlSearchClass.SelectedValue, ddlSearchSection.SelectedValue);
+        }
+
+        // Reset button
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            txtSearchName.Text = "";
+            ddlSearchClass.SelectedIndex = 0;
+            ddlSearchSection.SelectedIndex = 0;
+            BindStudentGrid();
+        }
+
+        // Edit
+        protected void gvStudents_RowEditing(object sender, System.Web.UI.WebControls.GridViewEditEventArgs e)
+        {
+            gvStudents.EditIndex = e.NewEditIndex;
+            BindStudentGrid(txtSearchName.Text.Trim(), ddlSearchClass.SelectedValue, ddlSearchSection.SelectedValue);
+        }
+
+        // Cancel Edit
+        protected void gvStudents_RowCancelingEdit(object sender, System.Web.UI.WebControls.GridViewCancelEditEventArgs e)
+        {
+            gvStudents.EditIndex = -1;
+            BindStudentGrid(txtSearchName.Text.Trim(), ddlSearchClass.SelectedValue, ddlSearchSection.SelectedValue);
+        }
+
+        // Update
+        protected void gvStudents_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int studentID = Convert.ToInt32(gvStudents.DataKeys[e.RowIndex].Value);
+            GridViewRow row = gvStudents.Rows[e.RowIndex];
+
+            string admissionno = ((TextBox)row.Cells[1].Controls[0]).Text.Trim();
+            string studentName = ((TextBox)row.Cells[2].Controls[0]).Text.Trim();
+            string className = ((TextBox)row.Cells[3].Controls[0]).Text.Trim();
+            string totalFees = ((TextBox)row.Cells[4].Controls[0]).Text.Trim();
+            string paidAmount = ((TextBox)row.Cells[5].Controls[0]).Text.Trim();
+            string pendingAmt = ((TextBox)row.Cells[6].Controls[0]).Text.Trim();
+
+            using (SqlConnection con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand(@"
+            UPDATE Add_Student SET
+                AdmissionNo = @AdmissionNo,
+                StudentName = @StudentName,
+                Class = @Class,
+                TotalFees = @TotalFees,
+                PaidAmount = @PaidAmount,
+                PendingAmount = @PendingAmount
+            WHERE StudentID = @StudentID", con);
+
+                cmd.Parameters.AddWithValue("@AdmissionNo", admissionno);
+                cmd.Parameters.AddWithValue("@StudentName", studentName);
+                cmd.Parameters.AddWithValue("@Class", className);
+                cmd.Parameters.AddWithValue("@TotalFees", totalFees);
+                cmd.Parameters.AddWithValue("@PaidAmount", paidAmount);
+                cmd.Parameters.AddWithValue("@PendingAmount", pendingAmt);
+                cmd.Parameters.AddWithValue("@StudentID", studentID);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            gvStudents.EditIndex = -1;
+            BindStudentGrid(txtSearchName.Text.Trim(),
+                            ddlSearchClass.SelectedValue,
+                            ddlSearchSection.SelectedValue);
+        }
+
+        protected void gvStudents_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "EditStudent")
+            {
+                int studentID = Convert.ToInt32(e.CommandArgument);
+
+                using (SqlConnection con = new SqlConnection(
+                    ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand(
+                        "SELECT * FROM Add_Student WHERE StudentID=@StudentID", con);
+
+                    cmd.Parameters.AddWithValue("@StudentID", studentID);
+                    con.Open();
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        hfStudentID.Value = studentID.ToString();
+                        txtaddno.Text = dr["AdmissionNo"].ToString();
+                        txtStudentName.Text = dr["StudentName"].ToString();
+                        txtClass.Text = dr["Class"].ToString();
+                        txtTotalFees.Text = dr["TotalFees"].ToString();
+                        txtPaidAmount.Text = dr["PaidAmount"].ToString();
+                        txtPaymentMode.Text = dr["PendingAmount"].ToString();
+                    }
+                }
+
+                // OPEN MODAL
+                ScriptManager.RegisterStartupScript(this, GetType(),
+                    "OpenModal", "$('#editModal').modal('show');", true);
+            }
+        }
+
+
+        protected void txtPaidAmount_TextChanged(object sender, EventArgs e)
+        {
+            decimal total = 0, paid = 0;
+
+            decimal.TryParse(txtTotalFees.Text, out total);
+            decimal.TryParse(txtPaidAmount.Text, out paid);
+
+            if (paid > total)
+            {
+                txtPaidAmount.Text = "";
+                txtPaymentMode.Text = total.ToString("0.00");
+            }
+            else
+            {
+                txtPaymentMode.Text = (total - paid).ToString("0.00");
+            }
+
+            ScriptManager.RegisterStartupScript(this, GetType(),
+                "open", "$('#editModal').modal('show');", true);
+        }
+
+        protected void btnUpdateStudent_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand(@"
+        UPDATE Add_Student SET
+            AdmissionNo = @AdmissionNo,
+            StudentName = @StudentName,
+            Class = @Class,
+            TotalFees = @TotalFees,
+            PaidAmount = @PaidAmount,
+            PendingAmount = @PendingAmount
+        WHERE StudentID = @StudentID", con);
+
+            cmd.Parameters.AddWithValue("@AdmissionNo", txtaddno.Text);
+            cmd.Parameters.AddWithValue("@StudentName", txtStudentName.Text);
+            cmd.Parameters.AddWithValue("@Class", txtClass.Text);
+            cmd.Parameters.AddWithValue("@TotalFees", txtTotalFees.Text);
+            cmd.Parameters.AddWithValue("@PaidAmount", txtPaidAmount.Text);
+            cmd.Parameters.AddWithValue("@PendingAmount", txtPaymentMode.Text);
+            cmd.Parameters.AddWithValue("@StudentID", hfStudentID.Value);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            BindStudentGrid();
+
+            ScriptManager.RegisterStartupScript(this, GetType(),
+                "close", "$('#editModal').modal('hide');", true);
+        }
+
+
+        // Delete
+        protected void gvStudents_RowDeleting(object sender, System.Web.UI.WebControls.GridViewDeleteEventArgs e)
+        {
+            int studentID = Convert.ToInt32(gvStudents.DataKeys[e.RowIndex].Value);
+
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SchoolDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("DELETE FROM Add_Student WHERE StudentID=@StudentID", con);
+                cmd.Parameters.AddWithValue("@StudentID", studentID);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            BindStudentGrid(txtSearchName.Text.Trim(), ddlSearchClass.SelectedValue, ddlSearchSection.SelectedValue);
+        }
+
+        protected void btnCloseModal_Click(object sender, EventArgs e)
+        {
+            ScriptManager.RegisterStartupScript(
+                this,
+                GetType(),
+                "CloseModal",
+                @"
+        var modalEl = document.getElementById('editModal');
+        var modal = bootstrap.Modal.getInstance(modalEl);
+        if (modal) {
+            modal.hide();
+        }
+        ",
+                true
+            );
+        }
+    }
+}
